343fd1ed43eaf1eef388aad1040b65ab589ffe5f
alfjoint <83755550+alfjoint@users.noreply.github.com>
2021-05-20 05:07:11 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("WAASSSSUUUUUUPPPPPP WORLD!!!!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
